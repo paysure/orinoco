1e7f4782f1dc05b7617a79ffa27e979d80ac6464
Numerlor <numerlor@numerlor.me>
2024-04-02 17:40:51 +0200

    FIX: new settings package

--- orinoco/settings.py
+++ orinoco/settings.py
@@ -1,10 +1,10 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
     IMPLICIT_TYPE_STRICT_MODE_ENABLED: bool = False
     CHAINING_TYPE_CHECK_STRICT_MODE_ENABLED: bool = False
-    VERBOSE_ERRORS = False
+    VERBOSE_ERRORS: bool = False
 
     class Config:
         env_prefix = "ORINOCO_"
